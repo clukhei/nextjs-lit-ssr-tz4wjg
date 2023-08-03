3face5fd095af49ae5d70180d13490482826c8aa
clukhei <55618945+clukhei@users.noreply.github.com>
2023-08-03 16:10:01 +0800

    use template css library

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,41 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
-import '../src/console-overview-template';
+// using css for template layout stylings
+import 'clk-css-templates/css/console.css';
+// using sgds-react library for the components
 import { Button, Form } from '@govtechsg/sgds-react';
 
 export default function Home() {
   return (
-    <div className={styles["console-template-container"]}>
-    <console-overview-template>
-      <span slot="title">Title of Page</span>
-      <span slot="action-button">
-        <Button slot="action-button" variant="info">
-        <i className="bi bi-plus-circle me-2"></i>
-          Create new product
-        </Button>
-      </span>
-      <span slot="search">
-        <Form>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-            <Form.Label>Search</Form.Label>
-            <Form.Control />
-          </Form.Group>
-        </Form>
-      </span>
-      <span slot="filter-button">
-        <Button variant="outline-dark">
-        <i className="bi bi-sliders me-2"></i>
-          Refine search
-        </Button>
-      </span>
-      <div slot="body" className={styles["console-over-template__slot_body"]}>
-        <h1>Section 1</h1>
-      </div>
-    </console-overview-template>
-    </div>
+    <console-template-overview>
+      <template-header>
+        <template-header-top>
+          <h1 className="title">Title of Page</h1>
+          <Button variant="info">
+            <i className="bi bi-plus-circle me-2"></i>Create new project
+          </Button>
+        </template-header-top>
+        <template-header-bottom>
+          <Form>
+            <Form.Group
+              classNameName="mb-3"
+              controlId="exampleForm.ControlInput1"
+            >
+              <Form.Label>Search</Form.Label>
+              <Form.Control />
+            </Form.Group>
+          </Form>
+          <Button variant="outline-dark">
+            {' '}
+            <i className="bi bi-sliders me-2"></i>
+            Refine search
+          </Button>
+        </template-header-bottom>
+      </template-header>
+      <template-body>
+        <body-section-one>
+          <h1>Section 1</h1>
+        </body-section-one>
+      </template-body>
+    </console-template-overview>
   );
 }
